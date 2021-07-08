d1d00d5f972fe3cd34fa80c3978050daf71b30d1
Sushant Bahirat <sushantbahirat40@gmail.com>
2021-07-08 14:05:26 +0530

    Add option to authenticate the user using google authentication and sign into the app

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const _ = require("lodash");
 const passport = require("passport");
 const session = require("express-session");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -41,26 +43,46 @@ mongoose.connect(URL+"userDB", {
 mongoose.set("useCreateIndex", true);
 
 //creating schema
-
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String
-    },
-    password: String
+    email: String,
+    password: String,
+    googleId: String
+
 });
 
 //to setup passport for monggose local to hash and salt the password
 UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(findOrCreate);
 
 //model
-const UserModel = new mongoose.model( "User", UserSchema); 
+const User = new mongoose.model( "User", UserSchema); 
 
 // to provide local login strategy to authenticate users
-passport.use(UserModel.createStrategy());
+passport.use(User.createStrategy());
 
 // to serialize and deserialize the user session
-passport.serializeUser(UserModel.serializeUser());
-passport.deserializeUser(UserModel.deserializeUser());
+passport.serializeUser((user, done)=> {
+    done(null, user.id);
+});
+  
+passport.deserializeUser((id, done)=> {
+    User.findById(id, (err, user)=> {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  (accessToken, refreshToken, profile, cb)=> {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+  }
+));
 
 //routes
 app.get("/", (req, res)=>{
@@ -74,6 +96,20 @@ app.get("/login", (req, res)=>{
 app.get("/register", (req, res)=>{
     res.render("register");
 });
+
+//google authentication
+app.get("/auth/google", 
+    //use passport to authenticate a user using google strategy and we want user's profile
+    passport.authenticate("google", {scope: ["profile"] })
+);
+
+//after authentication
+app.get("/auth/google/secrets", 
+    //if login fails, we redirect the user to login route
+    passport.authenticate("google", {failureRedirect: "/login"}),
+    //if login is successfull, we redirect the user to secrets route
+    (req,res)=>{ res.redirect("/secrets")}
+);
 
 app.get("/secrets", (req, res)=>{
     //if user is already authenticated (with the help of express-session, passport-local-mongoose)
@@ -90,7 +126,7 @@ app.get("/secrets", (req, res)=>{
 app.post("/register", (req, res)=>{
 
     //passport local mongoose package
-    UserModel.register({username: req.body.username}, req.body.password, (err, user)=>{
+    User.register({username: req.body.username}, req.body.password, (err, user)=>{
         if(err){
             console.log(err);
             res.status(409).redirect("/register");
@@ -108,7 +144,7 @@ app.post("/register", (req, res)=>{
 app.post("/login", (req, res)=>{
 
     //create a user
-    const user = new UserModel({
+    const user = new User({
         username: req.body.username,
         password: req.body.password
     });
